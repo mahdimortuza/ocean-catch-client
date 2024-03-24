320c855cf73acd238655e1295d5b794bf22e4425
mahdimortuza <mahdi.webx@gmail.com>
2024-03-24 13:34:36 +0600

    slider added

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+const assets = {
+  banners: {
+    banner1: require("./banner/banner1.png"),
+    banner2: require("./banner/banner2.png"),
+  },
+};
+export default assets;
--- src/components/pageSections/Homepage/Hero.tsx
+++ src/components/pageSections/Homepage/Hero.tsx
@@ -1,9 +1,40 @@
+"use client";
 import Container from "@/components/Container";
+import Autoplay from "embla-carousel-autoplay";
+
+import assets from "@/assets";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import { useRef } from "react";
 
 const HeroSection = () => {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
   return (
     <Container>
-      <h1>Hero</h1>
+      <Carousel
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent>
+          <CarouselItem className="flex justify-between">
+            <h1>Best deal online on SeaFood</h1>
+            <Image className="" src={assets.banners.banner1} alt="banner1" />
+          </CarouselItem>
+          <CarouselItem>
+            <h1>Get best SeaFood from us</h1>
+            <Image src={assets.banners.banner2} alt="banner2" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </Container>
   );
 };
