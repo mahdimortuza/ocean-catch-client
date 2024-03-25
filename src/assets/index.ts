bae10ab82265e17418d72ed9437466ddd5da17a2
mahdimortuza <mahdi.webx@gmail.com>
2024-03-25 16:19:16 +0600

    category added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,13 @@ const assets = {
     banner1: require("./banner/banner1.png"),
     banner2: require("./banner/banner2.png"),
   },
+  categories: {
+    lobster: require("./categories/lobster.jpg"),
+    insects: require("./categories/scallops.jpg"),
+    slat: require("./categories/bass.jpg"),
+    sweet: require("./categories/salmon.jpg"),
+    smallBig: require("./categories/shapper.jpg"),
+    squid: require("./categories/trout.jpg"),
+  },
 };
 export default assets;
--- src/components/pageSections/Homepage/CategorySection.tsx
+++ src/components/pageSections/Homepage/CategorySection.tsx
@@ -1,7 +1,68 @@
+import assets from "@/assets";
 import Container from "@/components/Container";
+import SectionTitle from "@/components/SectionTitle/SectionTitle";
+import Image from "next/image";
+import Link from "next/link";
 
 const CategorySection = () => {
-  return <Container>CategorySection</Container>;
+  const categories = [
+    {
+      img: assets.categories.lobster,
+      name: "Lobster and Shrimps",
+      link: "/",
+    },
+    {
+      img: assets.categories.insects,
+      name: "Caviar and Insects",
+      link: "/",
+    },
+    {
+      img: assets.categories.sweet,
+      name: "Sweet water fish",
+      link: "/",
+    },
+    {
+      img: assets.categories.slat,
+      name: "Salt water fish",
+      link: "/",
+    },
+    {
+      img: assets.categories.smallBig,
+      name: "Small & Big Fish",
+      link: "/",
+    },
+    {
+      img: assets.categories.squid,
+      name: "Squid and Crabs",
+      link: "/",
+    },
+  ];
+  return (
+    <Container className="py-10">
+      <SectionTitle
+        title="Shop from top categories"
+        highlight="top categories"
+        link="/"
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-5 mt-10 w-full mx-auto">
+        {categories.map((category) => (
+          <div className="mx-auto" key={category.img}>
+            <Link href={category.link}>
+              <Image
+                src={category.img}
+                width={200}
+                alt="category"
+                className="rounded-full border border-[#008ECC]"
+              />
+            </Link>
+            <h1 className="text-center text-base font-semibold mt-5">
+              {category.name}
+            </h1>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default CategorySection;
