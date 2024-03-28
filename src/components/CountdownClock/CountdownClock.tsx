be8575f672b956205fec7bd713bd1f9f11a65eaf
mahdimortuza <mahdi.webx@gmail.com>
2024-03-28 17:41:07 +0600

    clock added

---
+++ src/components/CountdownClock/CountdownClock.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const CountdownClock = () => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date("2024-04-30") - +new Date();
+    let timeLeft: {
+      days: number;
+      hours: number;
+      minutes: number;
+      seconds: number;
+    } = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const addLeadingZero = (value: number) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl text-red-500">Offer Ends in:</h1>
+
+      <div className="flex gap-1 mt-4">
+        <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
+          <span>
+            <span className="text-xl font-bold">
+              {addLeadingZero(timeLeft.days)}
+            </span>{" "}
+            <span>Day</span>
+          </span>
+        </div>
+
+        <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
+          <span>
+            <span className="text-xl font-bold">
+              {addLeadingZero(timeLeft.hours)}
+            </span>{" "}
+            <span>Hour</span>
+          </span>
+        </div>
+
+        <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
+          <span>
+            <span className="text-xl font-bold">
+              {addLeadingZero(timeLeft.minutes)}
+            </span>{" "}
+            <span>Min</span>
+          </span>
+        </div>
+
+        <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
+          <span>
+            <span className="text-xl font-bold">
+              {addLeadingZero(timeLeft.seconds)}
+            </span>{" "}
+            <span>Sec</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownClock;
--- src/components/pageSections/Homepage/FlashSaleSection.tsx
+++ src/components/pageSections/Homepage/FlashSaleSection.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/Container";
+import CountdownClock from "@/components/CountdownClock/CountdownClock";
 import FlashSellProductCard from "@/components/FlashSellProductCard/FlashSellProductCard";
 import SectionTitle from "@/components/SectionTitle/SectionTitle";
 import {
@@ -39,32 +40,7 @@ const FlashSaleSection = async () => {
       />
       <div className=" py-10 md:flex gap-5 items-center">
         <div className="w-[250px] mx-auto flex flex-col gap-1 px-1  text-center">
-          <h1>Flash sale offer</h1>
-          <div className="flex gap-1">
-            <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
-              <span>
-                <span className="text-xl font-bold">36</span> <span>Day</span>
-              </span>
-            </div>
-
-            <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
-              <span>
-                <span className="text-xl font-bold">36</span> <span>Hour</span>
-              </span>
-            </div>
-
-            <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
-              <span>
-                <span className="text-xl font-bold">36</span> <span>Min</span>
-              </span>
-            </div>
-
-            <div className="bg-[#505050] px-2 py-1 text-white rounded-md">
-              <span>
-                <span className="text-xl font-bold">36</span> <span>Sec</span>
-              </span>
-            </div>
-          </div>
+          <CountdownClock />
         </div>
 
         <div className="sm:w-[640px] md:w-[500px] xl:w-[1000px] mt-10 md:mt-0">
