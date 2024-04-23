ab4857cdb31b161065560795119b32d100dcdff2
mahdimortuza <mahdi.webx@gmail.com>
2024-04-23 12:41:25 +0600

    axios configured

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,9 +1,10 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 import { tagTypeList } from "../tag-types";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.base_api}` }),
+  baseQuery: axiosBaseQuery({ baseUrl: `${process.env.base_api}` }),
   endpoints: () => ({}),
   tagTypes: tagTypeList,
 });
