510c1a2568dddd5eae703d99d4f289fbc0f8862f
mahdimortuza <mahdi.webx@gmail.com>
2024-04-23 10:10:49 +0600

    redux added

---
+++ src/redux/tag-types.ts
@@ -0,0 +1,13 @@
+export enum tagTypes {
+  fishes = "fishes",
+  admin = "admin",
+  user = "user",
+  review = "review",
+}
+
+export const tagTypeList = [
+  tagTypes.fishes,
+  tagTypes.admin,
+  tagTypes.user,
+  tagTypes.review,
+];
