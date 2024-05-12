ca441f3a4cce656429a68054c3c7c4fe4e942130
mahdimortuza <mahdi.webx@gmail.com>
2024-05-12 10:50:46 +0600

    checkout page added

--- src/app/(dashboardLayout)/dashboard/my-orders/page.tsx
+++
@@ -1,9 +0,0 @@
-const MyOrderPage = () => {
-  return (
-    <div className="flex justify-center items-center h-[85vh]">
-      <h1 className="text-3xl text-red-500 font-semibold">my orders</h1>
-    </div>
-  );
-};
-
-export default MyOrderPage;
---
+++ src/app/(dashboardLayout)/dashboard/order-history/page.tsx
@@ -0,0 +1,9 @@
+const OrderHistoryPage = () => {
+  return (
+    <div className="flex justify-center items-center h-[85vh]">
+      <h1 className="text-3xl text-red-500 font-semibold">OrderHistoryPage</h1>
+    </div>
+  );
+};
+
+export default OrderHistoryPage;
